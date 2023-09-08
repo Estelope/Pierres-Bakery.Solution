b49391eb96dc8ca6bce16c6c3e63dd6c16047f35
Estelope <elopez14567@gmail.com>
2023-09-07 21:49:07 -0600

    3rd test good fail

--- Bakery.Tests/Model Tests/BakeryTests.cs
+++ Bakery.Tests/Model Tests/BakeryTests.cs
@@ -9,17 +9,28 @@ namespace Bakery.Tests
   {
 
     [TestMethod]
-    public void BreadConstructor_CreatesInstanceOfBread_Bread()
-    {
+      public void BreadConstructor_CreatesInstanceOfBread_Bread()
+      {
         Bread bread = new Bread();
         Assert.AreEqual(typeof(Bread) , bread.GetType());
     }
 
     [TestMethod]
-    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
-    {
+      public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+      {
         Pastry pastry = new Pastry();
         Assert.AreEqual(typeof(Pastry) , pastry.GetType());
     }
+
+    [TestMethod]
+      public void PastryConstructor_AcceptsInputValues()
+      {
+        int pricePerPastry = 2;
+        int pastriesPerDeal = 4;
+        Pastry pastry = new Pastry(pricePerPastry, pastriesPerDeal);
+        
+        Assert.AreEqual(pricePerPastry, pastry.PricePerPastry);
+        Assert.AreEqual(pastriesPerDeal, pastry.PastriesPerDeal);
+      }
   }
 }

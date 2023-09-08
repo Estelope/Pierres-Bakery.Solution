6cc1c111a86222545c3d477dc7f1170b11bf21bb
Estelope <elopez14567@gmail.com>
2023-09-08 01:03:24 -0600

    add functioning ui

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,7 +6,35 @@ namespace Bakery
     {
         static void Main()
         {
+            Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
             Console.WriteLine("Welcome to Pierre's Bakery!");
+            Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+            Bread bread = new Bread(5, 3);
+            Pastry pastry = new Pastry(2, 4);
+            Console.Write("Enter the number of loaves of Bread: ");
+                if (int.TryParse(Console.ReadLine(), out int breadQuantity))   //TryParse lets input validation happen wihtout exiting conosole
+            {
+                int breadCost = bread.CalculateBreadCost(breadQuantity);
+
+            Console.Write("Enter the number of Pastries: ");
+                if (int.TryParse(Console.ReadLine(), out int pastryQuantity))
+                {
+                    int pastryCost = pastry.CalculatePastryCost(pastryQuantity);
+                    int totalCost = BakeryUtility.CalculateTotalCost(breadCost, pastryCost);
+
+                    Console.WriteLine($"Total cost for Bread: ${breadCost}");
+                    Console.WriteLine($"Total cost for Pastries: ${pastryCost}");
+                    Console.WriteLine($"Total cost for the order: ${totalCost}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input for Pastries. Please enter a valid number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for Bread. Please enter a valid number.");
+            }
         }
     }
 }

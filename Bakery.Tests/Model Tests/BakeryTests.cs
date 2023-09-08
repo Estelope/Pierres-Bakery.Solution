5aa54d174cb9186f8a0f4646b90271af987623b3
Estelope <elopez14567@gmail.com>
2023-09-07 21:16:24 -0600

    1st test good fail, add basic ui

---
+++ Bakery.Tests/Model Tests/BakeryTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Bakery;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BakeryTests
+  {
+
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+        Bread bread = new Bread();
+       
+    }
+  }
+}
---
+++ Bakery/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Bakery
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Pierre's Bakery!");
+        }
+    }
+}
